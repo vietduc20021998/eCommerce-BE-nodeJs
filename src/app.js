42f881b0a1e162fcee07b022aa2765e5db366d15
bdvduc <bdvduc@rsupport.com>
2025-03-02 23:48:15 +0700

    Section 2: Node.js Backend Architecture - Những folders và packages cần thiết khi khởi tạo Project!

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const compression = require("compression");
+const express = require("express");
+const { default: helmet } = require("helmet");
+const morgan = require("morgan");
+const app = express();
+
+// init middlewares
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+
+// init db
+
+// init routes
+app.get("/", (req, res, next) => {
+  const strCompress = "Hello, world";
+
+  return res.status(200).json({
+    message: "Hehe, well, you",
+    metadata: strCompress.repeat(10000),
+  });
+});
+
+// handle error
+
+module.exports = app;
